14c7364755eb2a21f465a4d9ddaef41eaa310abd
waterflow80 <thessalonikaathena@outlook.com>
2023-02-28 11:58:36 +0100

    Spring REST Api

---
+++ spring-rest-api/src/test/java/com/tutorial/springrestapi/SpringRestApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.tutorial.springrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
